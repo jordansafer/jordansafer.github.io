ba9e9164bbc6f7a98bdc565fc57682c7d2001c33
Jordan Safer <jordansafer2@gmail.com>
2023-07-09 01:11:52 -0400

    basic styling and site pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => (
+  <div>
+    <h1>Welcome to my website</h1>
+    <p>This is the home page.</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h1>About Me</h1>
+    <p>I am an academic...</p>
+  </div>
+);
+
+const Contact = () => (
+  <div>
+    <h1>Contact Me</h1>
+    <p>You can reach me at...</p>
+  </div>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { page: 'home' };
+  }
+
+  navigate(page) {
+    this.setState({ page });
+  }
+
+  render() {
+    let Page;
+    switch (this.state.page) {
+      case 'home':
+        Page = Home;
+        break;
+      case 'about':
+        Page = About;
+        break;
+      case 'contact':
+        Page = Contact;
+        break;
+      default:
+        Page = Home;
+    }
+
+    /*return (
+      <div>
+        <nav>
+          <button onClick={() => this.navigate('home')}>Home</button>
+          <button onClick={() => this.navigate('about')}>About</button>
+          <button onClick={() => this.navigate('contact')}>Contact</button>
+        </nav>
+        <Page />
+      </div>
+    );*/
+
+    // new version with some css classes for styling and a comment explain
+    // what styling is brought by each class
+    return (
+      <div>
+        <nav className='navBar'>
+          <button className="navButton" onClick={() => this.navigate('home')}>Home</button>
+          <button className="navButton" onClick={() => this.navigate('about')}>About</button>
+          <button className="navButton" onClick={() => this.navigate('contact')}>Contact</button>
+        </nav>
+        <div className="sitePage">
+          <Page />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
