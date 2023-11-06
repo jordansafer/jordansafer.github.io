30f632d23eca4594247270fea0b7e21d9e63b7ae
Jordan Safer <jordansafer2@gmail.com>
2023-11-06 04:09:35 -0500

    Revert "Updates"
    This reverts commit a8f9726e228f866377bf4d3241a6ec0fbc810437.

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import Projects from './Projects';
+
+// Future sections:
+// Home: my picture and a little about me
+// Publications: (in the future if i have these)
+// Research: (maybe projects for now?)
+// Academics: Degrees and all Academic awards, tech skills?
+// CV: PDF CV
+// Contact: just email? not phone or address probably. maybe linkedin
+
+// TODO make each page have a separate path
+
+const Academics = () => (
+  <div>
+    <h1>Academics</h1>
+    <p>list degrees, awards section, tech skills, vertically</p>
+  </div>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { page: 'home' };
+  }
+
+  navigate(page) {
+    this.setState({ page });
+  }
+
+  render() {
+    let Page;
+    switch (this.state.page) {
+      case 'home':
+        Page = Home;
+        break;
+      case 'projects':
+        Page = Projects;
+        break;
+      default:
+        Page = Home;
+    }
+
+    // new version with some css classes for styling and a comment explain
+    // what styling is brought by each class
+    return (
+      <div>
+        <nav className='navBar'>
+          <button className="navButton" onClick={() => this.navigate('home')}>Home</button>
+          <button className="navButton" onClick={() => this.navigate('projects')}>Projects</button>
+        </nav>
+        <div className="sitePage">
+          <Page />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
