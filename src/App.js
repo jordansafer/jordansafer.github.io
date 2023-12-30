ebd3b9c4a5f6a9ff84717b6224437a07e25d4a4d
Jordan Safer <jordansafer2@gmail.com>
2023-12-30 17:16:59 -0500

    actual hash routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import './App.css';
 import Home from './Home';
